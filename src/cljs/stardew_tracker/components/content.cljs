4b84e85ce6f5c5cc44d0598b254c74980e256e30
Mitchel Burton <jaggdev@gmail.com>
2018-12-26 14:22:26 -0600

    Added more content to item cards
    Expanded item cards and accounted for multiple seasons and skills

--- src/cljs/stardew_tracker/components/content.cljs
+++ src/cljs/stardew_tracker/components/content.cljs
@@ -4,7 +4,18 @@
 (defn content []
   [:main
    [:div.items
-    (for [{:keys [id name source]} (vals @state/items)]
-      [:div.item {:key id}
-       [:p name]
-       [:p source]])]])
+    (for [{:keys [id name source seasons skills]} (vals @state/items)]
+      [:div.card {:key id}
+       [:header.card-header
+        [:p.card-header-title name]]
+       [:div.card-content
+        [:p source]
+        [:button "test"]]
+       [:div.card-footer
+        ;; have to use for to iterate through the seasons vector
+        (for [season seasons]
+          [:span season])]
+       [:div.card-footer
+        ;; have to use for to iterate through the skills vector
+        (for [skill skills]
+          [:span skill])]])]])

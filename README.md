aeebf170d21b5951ece06d72fad38f36d88ba64f
Mitchel Burton <jaggdev@gmail.com>
2018-12-26 11:52:10 -0600

    Added state, favicon, and updated readme
    Initial pass at creating game data pieces

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # stardew-tracker
 
+Big shoutout to John Cleaver (kihashi) for his work on the stardew community checklist.
+This is meant to be a re-write of his application in Clojure.
 
 ## Development
 
@@ -46,44 +48,6 @@ In the REPL, type
 ```
 
 Notice again how the browser updates.
-
-### Lighttable
-
-Lighttable provides a tighter integration for live coding with an inline
-browser-tab. Rather than evaluating cljs on the command line with the Figwheel
-REPL, you can evaluate code and preview pages inside Lighttable.
-
-Steps: After running `(go)`, open a browser tab in Lighttable. Open a cljs file
-from within a project, go to the end of an s-expression and hit Cmd-ENT.
-Lighttable will ask you which client to connect. Click 'Connect a client' and
-select 'Browser'. Browse to [http://localhost:10555](http://localhost:10555)
-
-View LT's console to see a Chrome js console.
-
-Hereafter, you can save a file and see changes or evaluate cljs code (without
-saving a file).
-
-### Emacs/CIDER
-
-CIDER is able to start both a Clojure and a ClojureScript REPL simultaneously,
-so you can interact both with the browser, and with the server. The command to
-do this is `M-x cider-jack-in-clojurescript`.
-
-We need to tell CIDER how to start a browser-connected Figwheel REPL though,
-otherwise it will use a JavaScript engine provided by the JVM, and you won't be
-able to interact with your running app.
-
-Put this in your Emacs configuration (`~/.emacs.d/init.el` or `~/.emacs`)
-
-``` emacs-lisp
-(setq cider-cljs-lein-repl
-      "(do (user/go)
-           (user/cljs-repl))")
-```
-
-Now `M-x cider-jack-in-clojurescript` (shortcut: `C-c M-J`, that's a capital
-"J", so `Meta-Shift-j`), point your browser at `http://localhost:10555`, and
-you're good to go.
 
 ## Testing
 
---
+++ src/cljs/stardew_tracker/state.cljs
@@ -0,0 +1,68 @@
+(ns stardew-tracker.state
+  (:require [reagent.core :as r]))
+
+;; Items game data
+(def items (r/atom
+              {:name "Wild Horseradish"
+               :skills ["foraging"]
+               :seasons ["spring"]
+               :id 0
+               :source "Foraging during Spring."
+               :bundles {:bundle_id 0
+                         :count 1
+                         :id 0}}
+              {:name "Daffodil"
+               :skills ["foraging"]
+               :seasons ["spring"]
+               :id 1
+               :source "Foraging during Spring. Buy from Pierre's Shop at the Flower Dance."
+               :bundles {:bundle_id 0
+                         :count 1
+                         :id 1}}
+              {:name "Leek"
+               :skills ["foraging"]
+               :seasons ["spring"]
+               :id 2
+               :source "Foraging during Spring."
+               :bundles {:bundle_id 0
+                         :count 1
+                         :id 2}}
+              {:name "Dandelion"
+               :skills ["foraging"]
+               :seasons ["spring"]
+               :id 3
+               :source "Foraging during Spring. Buy from Pierre's Shop at the Flower Dance"
+               :bundles {:bundle_id 0
+                         :count 1
+                         :id 3}}))
+
+;; Seasons game data
+(def seasons (r/atom {{:id "spring"
+                       :order 0
+                       :name "Spring"}
+                      {:id "summer"
+                       :order 1
+                       :name "Summer"}
+                      {:id "fall"
+                       :order 2
+                       :name "Fall"}
+                      {:id "winter"
+                       :order 3
+                       :name "Winter"}}))
+
+;; Rooms game data
+(def rooms (r/atom [{:id 0
+                     :name "Crafts Room"
+                     :reward "Bridge Repair"}]))
+
+;; Bundles game data
+(def bundles (r/atom {{:id 0
+                       :name "Spring Foraging Bundle"
+                       :room 0
+                       :reward "Spring Seeds (30)"
+                       :items_required 4}
+                      {:id 1
+                       :name "Summer Foraging Bundle"
+                       :room 0
+                       :reward "Summer Seeds (30)"
+                       :items_required 3}}))

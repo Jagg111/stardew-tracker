398c289e7c2bb70fdb7641707b50b3287424681d
Mitchel Burton <jaggdev@gmail.com>
2018-12-27 14:34:54 -0600

    Added styling
    Broke up the pieces of item cards a bit and gave them some initial TLC to get them looking better.

--- src/cljs/stardew_tracker/components/content.cljs
+++ src/cljs/stardew_tracker/components/content.cljs
@@ -5,32 +5,50 @@
   [:main
    [:div.items
     (for [{:keys [id name source seasons skills bundles]} (vals @state/items)]
-      [:div.card {:key id}
-       [:header.card-header
-        [:p.card-header-title name]]
+      [:div {:key id :class "card"}
+
+       ;; item card name
+       [:div {:class "card-content z-depth-1"}
+        [:div {:class "card-title"}
+         [:p name]]]
+
+       ;; item card source
        [:div.card-content
-        [:p source]
-        [:hr]
+        [:p {:class "grey-text text-darken-1"} source]]
+
+       ;; bundle tracker
+       [:div.card-action
         ;; if the current items "bundles_id" is present in the progress atom
         ;; this will eventually need to account for items that count towards multiple bundles
         (if (contains? @state/progress (get-in bundles [:id]))
           ;; true = that item is completed
           [:div
-           [:i.material-icons "check_box_"]
-           [:button "completed"]]
+           [:a {:class "btn-small"}
+            [:i {:class "material-icons left"} "check_box"]
+            "completed"]]
           ;; false = this is an open task
           [:div
-           [:i.material-icons "check_box_outline_blank"]
-           [:button "todo"]])
+           [:a {:class "btn-small"}
+            [:i {:class "material-icons left"} "check_box_outline_blank"]
+            "todo"]])
         ;; using the bundle_id on the item, create the key to look up the bundles name from the bundles atom
         [:span (get-in @state/bundles [(keyword (str "bundle_" (get-in bundles [:bundle_id]))) :name])]]
 
-
-       [:div.card-footer
+       ;; what season item can be found in
+       [:div {:class "card-content grey lighten-4"}
+        [:i {:class "material-icons"} "date_range"]
         ;; have to use for to iterate through the seasons vector
         (for [season seasons]
           [:span season])]
-       [:div.card-footer
+
+       ;; what skill the item can be acquired from
+       [:div {:class "card-content grey lighten-4"}
+        [:i {:class "material-icons"} "work"]
         ;; have to use for to iterate through the skills vector
         (for [skill skills]
-          [:span skill])]])]])
+          [:span skill])]
+       ]
+      )
+    ]
+   ]
+  )

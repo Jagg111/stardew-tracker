a8415fc63cd250fe64cb82f4606ffa4860257463
Mitchel Burton <jaggdev@gmail.com>
2019-01-25 13:27:47 -0600

    Updated header layout
    Made some minor tweaks to get the header looking better.

--- src/cljs/stardew_tracker/components/header.cljs
+++ src/cljs/stardew_tracker/components/header.cljs
@@ -1,11 +1,12 @@
 (ns stardew-tracker.components.header)
 
 (defn header []
-  [:header.grid-x.grid-padding-x
-   [:div#header-left.cell.auto
-    [:h1 "Stardew Community Checklist"]]
-   [:div#header-right.cell.shrink
-    [:a {:href "#"}
-     [:span [:i {:class "material-icons left"} "cloud_download"] "Export Data"]]
-    [:a {:href "#"}
-     [:span [:i {:class "material-icons left"} "cloud_upload"] "Import Data"]]]])
+  [:header
+   [:div#header-wrapper.grid-x.grid-padding-x
+    [:div#header-left.cell.auto
+     [:h1 "Stardew Community Checklist"]]
+    [:div#header-right.cell.shrink
+     [:a {:href "#"}
+      [:span [:i {:class "material-icons left"} "cloud_download"] "Export Data"]]
+     [:a {:href "#"}
+      [:span [:i {:class "material-icons left"} "cloud_upload"] "Import Data"]]]]])
